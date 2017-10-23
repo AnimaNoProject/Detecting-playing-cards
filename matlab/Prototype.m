180c8d2ae510fab96e3c6accd9b9c8895c725bf1
andi094 <32668872+andi094@users.noreply.github.com>
2017-10-23 14:08:56 +0200

    Experiment - Kantendetektion
    Gruppentreffen: Experiment Kantendetektion

--- matlab/Prototype.m
+++ matlab/Prototype.m
@@ -1,9 +1,33 @@
 % Prototype here please %
 
-input = imread('input/img01.jpg');
-tform = affine2d([1 0 0; .5 1 0; 0 0 1]); % transformations matrix?
-tinput = imwarp(input, tform);
+%1. Karten teilen/ 2 separate bilder erstellen mit je 1 Karte
+%2. Geometrisch projezieren / homogenes Bild erhalten
+%3. Template matching - resultat erhalten
 
+
+
+
+input = imread('input/img.jpg');
+tform = affine2d([1 0 0; .5 1 0; 0 0 1]); % transformations matrix?
+%tinput = imwarp(input, tform);
+%tinput = edge(input, 'Sobel');
+
+xsobelfilter = [-1 0 1; -2 0 2; -1 0 1];
+ysobelfilter = [-1 -2 -1; 0 0 0; 1 2 1] ;
+
+y = fspecial('sobel')
+
+tinput = imfilter(imfilter((input), xsobelfilter), ysobelfilter);
+%tinput(tinput > 200)  = 255;
+
+%Kmedian = medfilt2(tinput(1:2));
 imshow(input);
+imshow(Kmedian);
 figure();
-imshow(tinput);
+imshow(input(:,:,1));
+figure();
+imshow(input(:,:,2));
+figure();
+imshow(input(:,:,3));
+figure();
+imshow(rgb2gray(input));
