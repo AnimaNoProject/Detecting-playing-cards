bfc318abf2487a7cc3f3b18056772258cc1dcabd
janlaranjo <e1425799@student.tuwien.ac.at>
2017-11-06 11:14:31 +0100

    ecken erkennung
    Ecken werden teilweise erkannt

--- matlab/Prototype_jl.m
+++ matlab/Prototype_jl.m
@@ -8,12 +8,9 @@ inputcanny = edge(inputgrey, 'Canny', sigma);
 figure();
 imshow(inputcanny);
 %morphologie nutzen damit die kanten besser ausschauen
-
 se = strel('square',5);
 %se = strel('disk',10);
 inputmorph = imdilate(inputcanny,se);
-figure();
-imshow(inputmorph);
 %noise reduzieren
 inputfilt = medfilt2(inputmorph);
 %finde ecken
@@ -24,5 +21,37 @@ ysobelfilter = [-1 -2 -1; 0 0 0; 1 2 1] ;
 xsobel = imfilter(double(inputfilt), xsobelfilter);
 ysobel = imfilter(double(inputfilt), ysobelfilter);
 phi = rad2deg(atan2(xsobel, ysobel));
+%boundingbox
+boundary = regionprops(inputfilt, 'BoundingBox');
+boundingbox = boundary.BoundingBox;
+hold on;
+imshow(inputmorph);
+rectangle('Position',boundingbox,'EdgeColor','r','LineWidth',2);
+figure;
+%corner template bekommen
+templatecorner         = imread('input/corner_template.png');
+templategreycorner     = double(rgb2gray(templatecorner));
+templatecannycorner   = edge(templategreycorner, 'Canny', sigma);
+imshow(templatecannycorner);
+figure;
+%ecken  3 mal drehen
+templatecannycorner90 = rot90(templatecannycorner);
+templatecannycorner180 = rot90(templatecannycorner90);
+templatecannycorner270 = rot90(templatecannycorner180);
+%ecken erkennen mit template matching
+obenlinks   = normxcorr2(templatecannycorner, inputmorph);
+untenlinks   = normxcorr2(templatecannycorner90, inputmorph);
+untenrechts = normxcorr2(templatecannycorner180, inputmorph);
+obenrechts  = normxcorr2(templatecannycorner270, inputmorph);
+
+imshow(obenlinks);
+figure;
+imshow(untenlinks);
+figure;
+imshow(untenrechts);
+figure;
+imshow(obenrechts);
+figure;
 
 clc;
+clear;
