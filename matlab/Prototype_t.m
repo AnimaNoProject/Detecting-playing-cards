6f998bfec4dc1c2f7364da6502ed9ea4638b527c
AnimaNoProject <32396864+AnimaNoProject@users.noreply.github.com>
2017-11-24 22:28:47 +0100

    naives templatematching

--- matlab/Prototype_t.m
+++ matlab/Prototype_t.m
@@ -2,6 +2,8 @@
 input = imread('input/test_img.jpg');
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
+input = imresize(input, 0.5, 'nearest');
+
 [card_first, card_second] = splitCards(input);
 
 %Get the RGB card from the Original image by dotmultiplying with the binary
@@ -33,4 +35,23 @@ card_one = scaleCard(card_one);
 card_two = scaleCard(card_two);
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-imshowpair(card_one, card_two, 'Montage');
+template = imread('input/templ_pik_koenig.png');
+
+template = imresize(template, 0.5);
+
+% apply templete matching using power of the image
+result1=tmc(card_one, template);
+
+figure,
+subplot(2,2,1),imshow(template);title('Template');
+subplot(2,2,2),imshow(card_one);title('Target');
+subplot(2,2,3),imshow(result1);title('Matching Result using tmc');
+
+
+% apply templete matching using DC components of the image
+result2=tmc(card_two, template);
+
+figure,
+subplot(2,2,1),imshow(template);title('Template');
+subplot(2,2,2),imshow(card_two);title('Target');
+subplot(2,2,3),imshow(result2);title('Matching Result using tmc');
