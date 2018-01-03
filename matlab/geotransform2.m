124ea7c7dfeeec6f30936d2622b5c78c8dcff010
aleks <aleksandar.marinkovic@live.at>
2018-01-03 20:42:18 +0100

    kommentare

--- matlab/geotransform2.m
+++ matlab/geotransform2.m
@@ -1,17 +1,22 @@
 function  [newim] = geotransform2(im, T)
-%    im = rgb2gray(im);
-%    im = double(im/255);
+    
     [rows, cols] = size(im);
     x = (1:cols)';
     y = (1:rows)';
-
+    
+    % Alle möglichen xy-Koordinaten
     [xi, yi] = meshgrid(x,y);
     xi_size = length(xi(:));
+    
+    % mit der inversen Transformationsmatrix multiplizieren 
+    % \ statt multiplizieren, wird von matlab empfohlen läuft schneller
     imgT = T \ [xi(:) yi(:) ones(xi_size, 1)]';
-
+    
+    % xy-Koordinaten normalisieren
     x = imgT(1,:) ./ imgT(3,:);
     y = imgT(2,:) ./ imgT(3,:);
-
+    
+    % Interpolieren 
     newim = interp2(im, x, y);
     newim = reshape(newim, rows, cols);
 end
--- matlab/gettform2.m
+++ matlab/gettform2.m
@@ -1,19 +1,28 @@
+% mov - dort werden die fix Punkte hintransformiert
+% fix - 4 Ecken der Karte
 function H = gettform2(mov,fix)
-    npts=4;
-    x = mov(1,:);
-    y = mov(2,:);
+    
+    npts=4;         % Anzahl der Punkte
+    
+    % Koordinaten der Zieltransformation
+    x = mov(1,:);    
+    y = mov(2,:); 
+    % Koordinaten der Eckpunkte
     xd = fix(1,:);
     yd = fix(2,:);
-    H = zeros(2*npts,9);
     
+    % 8x9 Matrix mittels Direct Linear Transformation Algorithmus bilden (DLT)
+    H = zeros(2*npts,9);
     for i=1:npts
         H(2*i-1,:) = [x(i) y(i) 1 0 0 0 -xd(i).*x(i) -xd(i).*y(i) -xd(i)];
         H(2*i,:)   = [0 0 0 x(i) y(i) 1 -yd(i).*x(i) -yd(i).*y(i) -yd(i)]; 
     end
     
-    [U,S,V] = svd(H);
+    % Transformationsmatrix aus V entnehmen
+    [U,S,V] = svd(H); % H = U*S*V
     H=reshape(V(:,9),3,3)';
     
+    % Normalisieren
     S = svd(H);
     H = H / S(2);
 end
