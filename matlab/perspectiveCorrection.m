2be6a5f11d00d9bcb3e54751a46cc355fde975cc
aleks <aleksandar.marinkovic@live.at>
2018-01-05 22:00:49 +0100

    fixes

--- matlab/perspectiveCorrection.m
+++ matlab/perspectiveCorrection.m
@@ -9,14 +9,25 @@ function [orthogonalpersp_card] = perspectiveCorrection(img, firstcorner, second
     dright = pdist(topRight,'euclidean');
     % Kartenverhältnis 5:8
     if(dleft < dright)
-        base = [5 0; 0 0; 5 8; 0 8];
+        base = [5 0; 0 0; 5 8; 0 8]*150;
     else
-        base = [0 0; 0 8; 5 0; 5 8];  
+        base = [0 0; 0 8; 5 0; 5 8]*150;  
     end
     %%%%%%%%%%%%%%%%%%% Tansformation-Matrix %%%%%%%%%%%%%%%%%%%
     movPts  = [c r];
     fixPts  = base;
     tform   = gettform(movPts',fixPts');
 
-    orthogonalpersp_card = geotransform(img, tform);
+    img=double(img);
+    img=img/255;
+    % RGB-Kanäle einzeln transformieren
+    [r] = geotransform(img(:,:,1), tform);
+    [g] = geotransform(img(:,:,2), tform);
+    [b] = geotransform(img(:,:,3), tform);
+
+    % Output erzeugen und Kanäle zusammenlegen
+    orthogonalpersp_card = repmat(uint8(0),[size(r),3]);
+    orthogonalpersp_card(:,:,1) = uint8(round(r*255));
+    orthogonalpersp_card(:,:,2) = uint8(round(g*255));
+    orthogonalpersp_card(:,:,3) = uint8(round(b*255));
 end
