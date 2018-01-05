68612d7913ca0d4122167531683081d370c9350f
janlaranjo <e1425799@student.tuwien.ac.at>
2018-01-05 22:32:03 +0100

    Fast Fertig mit den Karten
    ....

---
+++ matlab/getResultOfMatch.m
@@ -0,0 +1,35 @@
+function result = getResultOfMatch(index_symbol_top, index_letter_top, index_symbol_bottom, index_letter_bottom)
+%   Author: Jan Michael Laranjo
+%   Detailed explanation:
+%   INPUT:
+%       index_symbol--> RGB output image of the geometric transformation
+%       index_letter--> RGB image. Basically a snippet of the original card.
+%                       Represents a symbol or a letter. 
+%   OUTPUT:
+%       result      --> is the textual based answer for this current round
+%        
+%   is responsible for the game logic.The method determines which card gives
+%   the 'Stich'.
+% Auswertung der Ergebnisvektoren und Ausgabe auf der Konsole
+
+% if (index_symbol == 1)
+%     Karo
+% elseif ( index_symbol == 2 )
+%     Herz
+% elseif ( index_symbol == 3 )
+%     Kreuz
+% elseif ( index_symbol == 4 )
+%     Pik
+% end
+% 
+% if (index_letter == 1)
+%     Ass
+% elseif ( index_letter == 2 )
+%     Zehn
+% elseif ( index_letter == 3 )
+%     Koenig
+% elseif ( index_letter == 4 )
+%     Dame
+% elseif ( index_letter == 5 )
+%     Bube
+end
--- matlab/main.m
+++ matlab/main.m
@@ -1,23 +1,36 @@
 %final implementaion
 function [result] = main(img_path)
-    image = imread(img_path);
-    %splitCards results are 2 binary images
-    %the first card is the bigger one
-    [first_binary_image, second_binary_image] = splitCards(image);
-    %get the colored result of the split cards
-    [first_rgb_image, second_rgb_image] = getSplitColoredCard(image, first_binary_image, second_binary_image);
-    %get grayscale images 
-%     first_gray_image    = rgb2gray(first_rgb_image);
-%     second_gray_image   = rgb2gray(second_rgb_image);
-    %correct the perspective of the 2 cards
-    [first_perspcorrected_card]     = correctPerspectives(first_rgb_image, first_binary_image, second_rgb_image, second_binary_image);
-    [second_perspcorrected_card]    = geom_transf_lowercard(first_binary_image,first_rgb_image);
+%   Author: Jan Michael Laranjo
+%   Detailed explanation:
+%   INPUT:
+%       img_path    --> RGB output image of the geometric transformation
+%   OUTPUT:
+%       result      --> is the textual based answer for this current round
+%   simulates a round of the game by reading the path of the picture.
+%   Afterwards the cards get splitted. The cards have a perspective
+%   projection and by correcting the perspective to orthogonal projection
+%   the values of the cards can be determined by template matching.
+%   Finally it can be determined a 'Stich' occured.
+%
+%read the image
+clc;
+image = imread(img_path);
+%splitCards results are 2 binary images
+%the first card is the bigger one
+[first_binary_image, second_binary_image] = splitCards(image);
+%get the colored result of the split cards
+[first_rgb_image, second_rgb_image] = getSplitColoredCard(image, first_binary_image, second_binary_image);
+%correct the perspective of the 2 cards
+[first_perspcorrected_card]     = correctPerspectives(first_rgb_image, first_binary_image, second_rgb_image, second_binary_image);
+[second_perspcorrected_card]    = geom_transf_lowercard(first_binary_image,first_rgb_image);
 
-    %start template matching
-    [index_symbol, index_letter] = decideCard(first_perspcorrected_card, 1);
-    
-    clc;
-    clear all;
+%start template matching to determine the value of the top and bottom
+%card
+[index_symbol_top, index_letter_top]        = decideCard(first_perspcorrected_card, 1);
+[index_symbol_bottom, index_letter_bottom]  = decideCard(second_perspcorrected_card, 0);
+
+%finally the result of the game can be determined
+result = getResultOfMatch(index_symbol_top, index_letter_top, index_symbol_bottom, index_letter_bottom);
 end
 
 function [first_rgb_image, second_rgb_image] = getSplitColoredCard(image, first_binary_image, second_binary_image)
